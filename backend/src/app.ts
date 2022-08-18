44d274bfa00bf1ecced3bb395ada273d68a35cfd
Thiago Soares <devthigosoares@gmail.com>
2022-08-17 22:50:31 -0400

    Feat: Endpoint criado

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,9 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import logger from 'morgan';
 
+import { router } from './router/index'
+
 export const app = express();
 
 // MIDDLEWARES
 app.use(express.json());
 app.use(cors());
 app.use(logger('dev'));
+
+// integrando endpoint na aplicação
+app.use('/', router);
