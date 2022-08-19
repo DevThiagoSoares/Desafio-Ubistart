858e0590e123e2f4fd46495c7731536a9884cb95
Thiago Soares <thiago.soares@grupoicts.com.br>
2022-08-19 11:34:34 -0400

    configuração de rotas

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -3,3 +3,4 @@ import { Router } from 'express';
 export const router = Router();
 
 router.get('/', (req, res) => res.send('API Desafiuo Ubistart'));
+router.get('/task', (req, res) => res.send('task'));
